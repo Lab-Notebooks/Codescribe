ca0d096072d4704a7989ea55b7b46535ff831a12
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2026-02-19 19:10:23 -0600

    small tweak for argo

--- code_scribe/lib/_llm.py
+++ code_scribe/lib/_llm.py
@@ -117,7 +117,7 @@ class ArgoModel:
             "prompt": [prompt_text],
             "stop": [],
             "temperature": 0.1,
-            "top_p": 0.9,
+            #"top_p": 0.9,
         }
 
         response = requests.post(
