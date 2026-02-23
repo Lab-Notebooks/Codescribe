92ffaecc344d7fff4fba312fb9193f34ba60f242
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2026-02-22 21:35:58 -0800

    Update package metadata for version and authors

--- code_scribe/__meta__.py
+++ code_scribe/__meta__.py
@@ -1,8 +1,8 @@
 """Metadata for CodeScribe package"""
 
 __pkgname__ = "CodeScribe"
-__version__ = "2025.dev"
-__authors__ = "Akash Dhruv"
+__version__ = "2026.dev"
+__authors__ = "Akash Dhruv & Codescribe Contributors"
 __license__ = "Apache License"
-__copyright__ = "Copyright (c) Akash Dhruv 2024. All Rights Reserved."
+__copyright__ = "Copyright (c) Akash Dhruv & Codescribe Contributors 2026. All Rights Reserved."
 __description__ = "Code Translation Utility"
