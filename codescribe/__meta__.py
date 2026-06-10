802130d4b8ecdf51dcb8b3953104c09eb8618579
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2026-06-09 23:19:30 -0500

    Fix case sensitivity and copyright

--- codescribe/__meta__.py
+++ codescribe/__meta__.py
@@ -1,8 +1,8 @@
 """Metadata for Codescribe package"""
 
-__pkgname__ = "Codescribe"
+__pkgname__ = "CodeScribe"
 __version__ = "2026.dev"
-__authors__ = "Akash Dhruv & Codescribe Contributors"
+__authors__ = "CodeScribe Contributors"
 __license__ = "Apache 2.0"
-__copyright__ = "Copyright (c) Akash Dhruv & Codescribe Contributors 2026. All Rights Reserved."
-__description__ = "Code Translation Utility"
+__copyright__ = "Copyright (c) CodeScribe Contributors 2026. All Rights Reserved."
+__description__ = "Coding Agent and Code Translation Utility"
